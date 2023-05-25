ee48c1a43591beec9ab51b56558cbe34ea7e5aa5
arcaydian <130350819+arcaydian@users.noreply.github.com>
2023-05-25 17:53:52 +0530

    Update Builder.java

--- creational/Builder.java
+++ creational/Builder.java
@@ -1,5 +1,31 @@
 class BuilderPattern {
   public static void main(String[] args) {
-    Car car = new CarBuilder();
+    Car car = new CarBuilder().setType("Sedan").setSeater(5).build();
+  }
+}
+
+interface Car {
+  String setType();
+  int setSeater();
+}
+
+class CarBuilder implements Car {
+  String type = "";
+  int seats = 0;
+  
+  Car car = new CarBuilder();
+  
+  @Override
+  String setType(String type) {
+    this.type = type;
+  }
+  
+  @Override
+  int setSeater(int seats) {
+    this.seats = seat;
+  }
+  
+  Car build() {
+    return this;
   }
 }
